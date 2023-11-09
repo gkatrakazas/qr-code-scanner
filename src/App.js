83c9443cae270e5180958a8e36e433049c2a9848
gkatrakazas <grigoriskatrakazas@gmail.com>
2023-11-09 11:15:35 +0200

    customize the main page

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import logo from './logo.svg';
 import QRCodeScanner from './QRCodeScanner'; // Replace with the actual import path
-import React, { useState, useEffect } from 'react';
-import {BsQrCodeScan} from 'react-icons/bs'
+import React, { useState } from 'react';
+import { BsQrCodeScan } from 'react-icons/bs';
 
 function App() {
+  // QR Code part
+  const [isQRScannerOpen, setQRScannerOpen] = useState(false);
 
-  	// QR Code part
-	const [isQRScannerOpen, setQRScannerOpen] = useState(false);
+  const openQRScanner = () => {
+    setQRScannerOpen(true);
+  };
 
-	const openQRScanner = () => {
-		setQRScannerOpen(true);
-	};
+  const closeQRScanner = () => {
+    setQRScannerOpen(false);
+  };
 
-	const closeQRScanner = () => {
-		setQRScannerOpen(false);
-	};
   return (
-    <div>
-      					<button
-						className="px-2 py-2 mb-2 text-white bg-gray-600 hover:bg-custom-blue-hover focus:ring-4 focus:outline-none focus:ring-custom-blue font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-custom-blue-hover dark:hover:bg-custom-blue-hover dark:focus:ring-custom-blue-hover"
-						onClick={openQRScanner} // Open the QR code scanner modal
-					>
-						<div className="flex items-center">
-							<BsQrCodeScan size={20} className="text-white mr-2 sm:inline" />
-							<span className="sm:inline">Scan</span>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
+      <div className="text-center text-white">
+        <h1 className="text-4xl font-bold">Welcome to Fancy QR Scanner</h1>
+        <p className="text-lg mt-2 mb-6">Scan QR codes with style!</p>
 
-						</div>
+        <button
+          className="px-4 py-2 text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-400 font-medium rounded-lg text-lg"
+          onClick={openQRScanner} // Open the QR code scanner modal
+        >
+          <div className="flex items-center">
+            <BsQrCodeScan size={24} className="text-white mr-2" />
+            <span>Scan QR Code</span>
+          </div>
+        </button>
 
-					</button>
-
-          		{/* QR Code Scanner Modal */}
-			{isQRScannerOpen && (
-				<div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
-        	<QRCodeScanner
-          	onClose={closeQRScanner}
-					/>
-				</div>
-			)}
+        {/* QR Code Scanner Modal */}
+        {isQRScannerOpen && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+            <QRCodeScanner onClose={closeQRScanner} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
