f1579c51236054815914b86fb6de38a47212fcd2
gkatrakazas <grigoriskatrakazas@gmail.com>
2024-04-14 18:58:27 +0300

    add two more way of resolution

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import React, { useState } from 'react';
 import { BsQrCodeScan } from 'react-icons/bs';
 
 import QRCodeScanner2 from './QRCodeScanner/QRCodeScanner2';
+import QRCodeScanner3 from './QRCodeScanner/QRCodeScanner3';
+import QRCodeScanner4 from './QRCodeScanner/QRCodeScanner4';
 
 function App() {
   // QR Code part
   const [isQRScannerOpen, setQRScannerOpen] = useState(false);
   const [isQRScannerOpen2, setQRScannerOpen2] = useState(false);
+  const [isQRScannerOpen3, setQRScannerOpen3] = useState(false);
+  const [isQRScannerOpen4, setQRScannerOpen4] = useState(false);
 
   const openQRScanner = () => {
     setQRScannerOpen(true);
@@ -31,25 +35,59 @@ function App() {
         <h1 className="text-4xl font-bold">Welcome to Fancy QR Scanners</h1>
         <p className="text-lg mt-2 mb-6">Scan QR codes with style!</p>
 
-        <button
-          className="px-4 py-2 text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-400 font-medium rounded-lg text-lg"
-          onClick={openQRScanner} // Open the QR code scanner modal
-        >
-          <div className="flex items-center">
-            <BsQrCodeScan size={24} className="text-white mr-2" />
-            <span>Scan QR Code</span>
+        <div className="space-y-4 text-white">
+          <div className="p-4 border rounded-lg shadow-md flex flex-col items-center">
+            <h2 className="text-lg font-medium mb-2">Standard Scanner</h2>
+            <p>QR Scan library: jsQR</p>
+            <p>Resolution: max possible</p>
+            <button
+              className="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-400 font-medium rounded-lg text-lg text-white flex items-center"
+              onClick={openQRScanner}
+            >
+              <BsQrCodeScan size={24} className="mr-2" />
+              Scan QR Code
+            </button>
           </div>
-        </button>
 
-        <button
-          className="px-4 py-2 text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-400 font-medium rounded-lg text-lg"
-          onClick={openQRScanner2} // Open the QR code scanner modal
-        >
-          <div className="flex items-center">
-            <BsQrCodeScan size={24} className="text-white mr-2" />
-            <span>Scan QR Code2</span>
+          <div className="p-4 border rounded-lg shadow-md flex flex-col items-center">
+            <h2 className="text-lg font-medium mb-2">Advanced Scanner 2</h2>
+            <p>QR Scan library: qr-scanner</p>
+            <p>Resolution:(ideal:max possible)</p>
+            <button
+              className="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-400 font-medium rounded-lg text-lg text-white flex items-center"
+              onClick={openQRScanner2}
+            >
+              <BsQrCodeScan size={24} className="mr-2" />
+              Scan QR Code2
+            </button>
           </div>
-        </button>
+
+          <div className="p-4 border rounded-lg shadow-md flex flex-col items-center">
+            <h2 className="text-lg font-medium mb-2">Experimental Scanner 3</h2>
+            <p>QR Scan library: qr-scanner</p>
+            <p>Resolution:(ideal: 1920)</p>
+            <button
+              className="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-400 font-medium rounded-lg text-lg text-white flex items-center"
+              onClick={() => setQRScannerOpen3(true)}
+            >
+              <BsQrCodeScan size={24} className="mr-2" />
+              Scan QR Code3
+            </button>
+          </div>
+
+          <div className="p-4 border rounded-lg shadow-md flex flex-col items-center">
+            <h2 className="text-lg font-medium mb-2">Experimental Scanner 4</h2>
+            <p>QR Scan library: qr-scanner</p>
+            <p>Resolution:(min: 720, ideal: 1280, max: max possible)</p>
+            <button
+              className="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-400 font-medium rounded-lg text-lg text-white flex items-center"
+              onClick={() => setQRScannerOpen4(true)}
+            >
+              <BsQrCodeScan size={24} className="mr-2" />
+              Scan QR Code4
+            </button>
+          </div>
+        </div>
 
         {/* QR Code Scanner Modal */}
         {isQRScannerOpen && (
@@ -63,6 +101,17 @@ function App() {
             <QRCodeScanner2 onClose={closeQRScanner2} />
           </div>
         )}
+        {isQRScannerOpen3 && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+            <QRCodeScanner3 onClose={() => setQRScannerOpen3(false)} />
+          </div>
+        )}
+        {isQRScannerOpen4 && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+            <QRCodeScanner4 onClose={() => setQRScannerOpen4(false)} />
+          </div>
+        )}
+
       </div>
     </div>
   );
---
+++ src/QRCodeScanner/QRCodeScanner3.js
@@ -0,0 +1,298 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Webcam from 'react-webcam';
+import jsQR from 'jsqr';
+import { BsQrCodeScan } from 'react-icons/bs';
+import { PiCameraRotateFill } from 'react-icons/pi';
+import Spinner from '../Spinner';
+import { useTranslation } from 'react-i18next';
+import { FaCheckCircle } from "react-icons/fa";
+import CornerBox from './CornerBox';
+import ScanningLine from './ScanningLine';
+import { RiZoomInFill, RiZoomOutFill } from "react-icons/ri";
+import QrScanner from 'qr-scanner';
+
+const QRScanner = ({ onClose }) => {
+
+	const [devices, setDevices] = useState([]);
+	const [bestCameraResolutions, setBestCameraResolutions] = useState({ front: null, back: null });
+	const webcamRef = useRef(null);
+	const [cameraReady, setCameraReady] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [currentDeviceIndex, setCurrentDeviceIndex] = useState(0);
+	const [qrDetected, setQrDetected] = useState(false);
+	const [boxSize, setBoxSize] = useState(null);
+	const [zoomLevel, setZoomLevel] = useState(1);
+	const [hasCameraPermission, setHasCameraPermission] = useState(null);
+	const { t } = useTranslation();
+
+	const handleZoomChange = (event) => {
+		const newZoomLevel = Number(event.target.value);
+		setZoomLevel(newZoomLevel);
+	};
+
+	const handleZoomIn = () => {
+		setZoomLevel(prevZoomLevel => Math.min(prevZoomLevel + 0.2, 3));
+	};
+
+	const handleZoomOut = () => {
+		setZoomLevel(prevZoomLevel => Math.max(prevZoomLevel - 0.2, 1));
+	};
+
+	const handleClose = () => {
+		onClose();
+	};
+
+	useEffect(() => {
+		navigator.mediaDevices.getUserMedia({ video: true })
+			.then(stream => {
+				setHasCameraPermission(true);
+				stream.getTracks().forEach(track => track.stop());
+			})
+			.catch(error => {
+				console.error("Camera access denied:", error);
+				setHasCameraPermission(false);
+			});
+	}, []);
+
+
+	useEffect(() => {
+		if (hasCameraPermission) {
+			navigator.mediaDevices.enumerateDevices()
+				.then(async mediaDevices => {
+					const videoDevices = mediaDevices.filter(({ kind }) => kind === "videoinput");
+
+					let bestFrontCamera = null;
+					let bestBackCamera = null;
+
+					for (const device of videoDevices) {
+						const stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: device.deviceId } });
+						const track = stream.getVideoTracks()[0];
+						const capabilities = track.getCapabilities();
+						const isBackCamera = device.label.toLowerCase().includes('back');
+						const resolution = {
+							width: capabilities.width?.max || 0,
+							height: capabilities.height?.max || 0
+						};
+
+						if (isBackCamera && (!bestBackCamera || bestBackCamera.resolution.width * bestBackCamera.resolution.height < resolution.width * resolution.height)) {
+							bestBackCamera = { device, resolution };
+						} else if (!isBackCamera && (!bestFrontCamera || bestFrontCamera.resolution.width * bestFrontCamera.resolution.height < resolution.width * resolution.height)) {
+							bestFrontCamera = { device, resolution };
+						}
+
+						track.stop();
+					}
+
+					const filteredDevices = [];
+					if (bestFrontCamera) {
+						filteredDevices.push(bestFrontCamera.device);
+					}
+					if (bestBackCamera) {
+						filteredDevices.push(bestBackCamera.device);
+					}
+
+					setBestCameraResolutions({
+						front: bestFrontCamera ? bestFrontCamera.resolution : null,
+						back: bestBackCamera ? bestBackCamera.resolution : null,
+					});
+
+					setDevices(filteredDevices);
+
+					const backCameraIndex = filteredDevices.findIndex(device =>
+						device.label.toLowerCase().includes('back'));
+
+					if (backCameraIndex !== -1) {
+						setCurrentDeviceIndex(backCameraIndex);
+					} else {
+						setCurrentDeviceIndex(0);
+					}
+					setCameraReady(true);
+				})
+				.catch(error => {
+					console.error("Error enumerating devices:", error);
+				});
+		}
+	}, [hasCameraPermission]);
+
+	const stopMediaTracks = (stream) => {
+		stream.getTracks().forEach(track => {
+			track.stop();
+		});
+	};
+
+	const switchCamera = () => {
+		if (devices.length > 1) {
+			const newIndex = (currentDeviceIndex + 1) % devices.length;
+			if (webcamRef.current && webcamRef.current.stream) {
+				stopMediaTracks(webcamRef.current.stream);
+			}
+			setCurrentDeviceIndex(newIndex);
+		}
+	};
+
+
+	const onUserMedia = () => {
+
+		if (webcamRef.current && webcamRef.current.video) {
+
+			const videoElement = webcamRef.current.video;
+			const qrScanner = new QrScanner(videoElement, (result) => {
+				console.log('decoded qr code:', result);
+				setQrDetected(true);
+				// Redirect to the URL found in the QR code
+				const scannedUrl = result.data;
+				setTimeout(() => {
+					setLoading(true);
+				}, 3000);
+				setTimeout(() => {
+					const baseUrl = window.location.origin;
+					const params = scannedUrl.split('?');
+					const cvUrl = `${baseUrl}/cb?${params[1]}&wwwallet_camera_was_used=true`;
+					window.location.href = cvUrl;
+				}, 1000);
+			}, { highlightScanRegion: true, highlightCodeOutline: true });
+
+			qrScanner.start().catch(err => {
+				console.error('Error starting QR Scanner: ', err);
+				// Optionally update UI or state to reflect the error
+			});
+
+			return () => {
+				qrScanner.stop();
+				qrScanner.destroy();
+			};
+		}
+	};
+
+	const currentCameraType = devices[currentDeviceIndex]?.label.toLowerCase().includes('back') ? 'back' : 'front';
+	const maxResolution = bestCameraResolutions[currentCameraType];
+
+	let idealWidth, idealHeight;
+	if (maxResolution) {
+		console.log(maxResolution);
+
+		// Determine the smaller dimension to be the basis for square dimensions
+		let smallerDimension = Math.min(maxResolution.width, maxResolution.height);
+
+		// Cap the dimension at 1920 if it exceeds this value
+		if (smallerDimension > 1920) {
+			idealWidth = 1920;
+			idealHeight = 1920;
+		} else {
+			idealWidth = smallerDimension;
+			idealHeight = smallerDimension;
+		}
+	}
+
+	return (
+		<div className="fixed inset-0 flex items-center justify-center z-50">
+			<div className="absolute inset-0 bg-black opacity-50"></div>
+
+			{hasCameraPermission === false ? (
+				<div className="bg-white p-4 rounded-lg shadow-lg w-full lg:w-[33.33%] sm:w-[66.67%] z-10 relative m-4">
+					<div className="flex items-start justify-between border-b rounded-t dark:border-gray-600">
+						<h2 className="text-lg font-bold mb-2 text-custom-blue">
+							<BsQrCodeScan size={20} className="inline mr-1 mb-1" />
+							{t('qrCodeScanner.title')}
+						</h2>
+
+						<button
+							type="button"
+							className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+							onClick={handleClose}
+						>
+							<svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+								<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+							</svg>
+						</button>
+					</div>
+					<hr className="mb-2 border-t border-custom-blue/80" />
+					<p className='text-red-600'>
+						{t('qrCodeScanner.cameraPermissionAllow')}
+					</p>
+				</div>
+			) : (!cameraReady || loading) ? (
+				<div className="flex items-center justify-center h-24">
+					<Spinner />
+				</div>
+			) : (
+				<div className="bg-white p-4 rounded-lg shadow-lg w-full lg:w-[33.33%] sm:w-[66.67%] z-10 relative m-4">
+					<div className="flex items-start justify-between border-b rounded-t dark:border-gray-600">
+						<h2 className="text-lg font-bold mb-2 text-custom-blue">
+							<BsQrCodeScan size={20} className="inline mr-1 mb-1" />
+							{t('qrCodeScanner.title')}
+						</h2>
+
+						<button
+							type="button"
+							className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+							onClick={handleClose}
+						>
+							<svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+								<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+							</svg>
+						</button>
+					</div>
+					<hr className="mb-2 border-t border-custom-blue/80" />
+					<p className="italic pd-2 text-gray-700">
+						{t('qrCodeScanner.description')}
+					</p>
+					<div className="webcam-container" style={{ position: 'relative', overflow: 'hidden' }}>
+						<Webcam
+							audio={false}
+							ref={webcamRef}
+							screenshotFormat="image/jpeg"
+							videoConstraints={{
+								deviceId: devices[currentDeviceIndex]?.deviceId,
+								height: { ideal: idealHeight },
+								width: { ideal: idealWidth }
+							}}
+							style={{ height: 'auto', transform: `scale(${zoomLevel})`, transformOrigin: 'center', width: '100%', }}
+							onUserMedia={onUserMedia}
+						/>
+						{boxSize && (
+							<>
+								<CornerBox qrDetected={qrDetected} side="borderLeft" position="borderTop" boxSize={boxSize} />
+								<CornerBox qrDetected={qrDetected} side="borderRight" position="borderTop" boxSize={boxSize} />
+								<CornerBox qrDetected={qrDetected} side="borderLeft" position="borderBottom" boxSize={boxSize} />
+								<CornerBox qrDetected={qrDetected} side="borderRight" position="borderBottom" boxSize={boxSize} />
+								<ScanningLine qrDetected={qrDetected} boxSize={boxSize} />
+							</>
+						)}
+						{qrDetected && (
+							<div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+								<FaCheckCircle size={100} color="green" />
+							</div>
+						)}
+					</div>
+					<div className='flex justify-between align-center'>
+						<div className="flex items-center my-4 pr-4 w-full">
+							<RiZoomOutFill className="text-gray-400 mr-2 mt-2" onClick={handleZoomOut} size={35} />
+							<input
+								type="range"
+								min="1"
+								max="3"
+								step="0.1"
+								value={zoomLevel}
+								onChange={handleZoomChange}
+								className="w-full h-2 bg-gray-200 rounded-lg cursor-pointer dark:bg-gray-700 mt-2"
+							/>
+							<RiZoomInFill className="text-gray-400 ml-2 mt-2" onClick={handleZoomIn} size={35} />
+						</div>
+						{devices.length > 1 && (
+							<button
+								type="button"
+								className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm ml-4 p-2 my-2"
+								onClick={switchCamera}
+							>
+								<PiCameraRotateFill size={25} />
+							</button>
+						)}
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default QRScanner;
